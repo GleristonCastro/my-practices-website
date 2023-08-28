663e488e621daa8cbc087768110f5b1cd9468bc0
Gleriston <xpanium@gmail.com>
2023-08-28 17:41:43 -0300

    add: script scroll active link

--- 4_lamp_website/assets/js/main.js
+++ 4_lamp_website/assets/js/main.js
@@ -74,10 +74,31 @@ const toggleItem = (item) => {
   }
 };
 
-const scrollUp = () => {
+const scrollToTop = () => {
   const scrollUp = document.getElementById('scroll-up');
 
   this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
 };
 
-window.addEventListener('scroll', scrollUp);
+window.addEventListener('scroll', scrollToTop);
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollVertical = window.scrollY;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop -58;
+    const sectionId = current.getAttribute('id');
+    const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+    if(scrollVertical > sectionTop && scrollVertical <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    };
+  });
+};
+
+window.addEventListener('scroll', scrollActive);
