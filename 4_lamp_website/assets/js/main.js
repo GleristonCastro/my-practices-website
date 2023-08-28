6e1c8c830a18e40708395dee871e6583308f6779
Gleriston <xpanium@gmail.com>
2023-08-28 17:15:19 -0300

    add scripts scrollbar and scrollup of project

--- 4_lamp_website/assets/js/main.js
+++ 4_lamp_website/assets/js/main.js
@@ -73,3 +73,11 @@ const toggleItem = (item) => {
     item.classList.add('faq-open');
   }
 };
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+};
+
+window.addEventListener('scroll', scrollUp);
